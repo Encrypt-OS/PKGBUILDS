c6112880467c5a2afbd1a632024f0934af67011d
SnailDOS <zarasoonich@hotmail.com>
2022-04-16 23:35:02 +0800

    Update auto builder, add SnailDOS gpg keys

--- automate-package-build.sh
+++ automate-package-build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # import the GPG keys required to build some of the packages
 gpg --recv-keys FAEDBC4FB5AA3B17
+
 gpg --recv-keys BB463350D6EF31EF
+
+# SnailDOS Official Key
+gpg --recv-keys 585B7DE5EA37FBD6
+
 # Build
-for d in ./*/ ; do (cd "$d" && makepkg -si --sign --key 0ED1F6AD55DA600A858229D1181BBE63EF572041); done
+for d in ./*/ ; do (cd "$d" && makepkg -si --sign --key 585B7DE5EA37FBD6); done
 # Cleanup
 for d in ./*/ ; do (cd "$d" && makepkg --clean); done
