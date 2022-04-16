4e0967418f5a2f674086336f209a4c3e5ddeb2bb
SnailDOS <zarasoonich@hotmail.com>
2022-04-16 16:33:36 +0800

    Update calamares

--- automate-package-build.sh
+++ automate-package-build.sh
@@ -2,5 +2,7 @@
 # import the GPG keys required to build some of the packages
 gpg --recv-keys FAEDBC4FB5AA3B17
 gpg --recv-keys BB463350D6EF31EF
-
+# Build
 for d in ./*/ ; do (cd "$d" && makepkg -si); done
+# Cleanup
+for d in ./*/ ; do (cd "$d" && makepkg --clean); done
