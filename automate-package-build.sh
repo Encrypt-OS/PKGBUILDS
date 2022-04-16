05d499e17701789181908e0362271910d8b68509
SnailDOS <zarasoonich@hotmail.com>
2022-04-16 17:51:50 +0800

    Update build and gitignore

--- automate-package-build.sh
+++ automate-package-build.sh
@@ -3,6 +3,6 @@
 gpg --recv-keys FAEDBC4FB5AA3B17
 gpg --recv-keys BB463350D6EF31EF
 # Build
-for d in ./*/ ; do (cd "$d" && makepkg -si); done
+for d in ./*/ ; do (cd "$d" && makepkg -si --sign --key 0ED1F6AD55DA600A858229D1181BBE63EF572041); done
 # Cleanup
 for d in ./*/ ; do (cd "$d" && makepkg --clean); done
