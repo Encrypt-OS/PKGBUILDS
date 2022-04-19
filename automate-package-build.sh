0e76505032604dce2fc94ae602d0bb74694e281c
SnailDOS <46210117+snaildos@users.noreply.github.com>
2022-04-19 16:07:56 +0800

    Update automate-package-build.sh

--- automate-package-build.sh
+++ automate-package-build.sh
@@ -5,7 +5,7 @@ gpg --recv-keys FAEDBC4FB5AA3B17
 gpg --recv-keys BB463350D6EF31EF
 
 # SnailDOS Official Key
-gpg --recv-keys 585B7DE5EA37FBD6
+gpg --recv-keys AEA41F137D60202D1A2D3EB786CD105462642978
 
 # Build
 for d in ./*/ ; do (cd "$d" && makepkg -si --sign --key 585B7DE5EA37FBD6); done
