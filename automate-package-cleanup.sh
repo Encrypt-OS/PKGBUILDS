599931352b4ba74929994fbc77f3746ce7279c83
SnailDOS <46210117+snaildos@users.noreply.github.com>
2022-05-09 21:33:50 +0800

    Update configuration & Cleanup script

--- automate-package-cleanup.sh
+++ automate-package-cleanup.sh
@@ -2,6 +2,7 @@
 
 # Remove src folder
 for d in ./*/ ; do (cd "$d" && sudo rm src -R); done
+for d in ./*/ ; do (cd "$d" && sudo rm pkg -R); done
 # Remove zst and sig files
 for d in ./*/ ; do (cd "$d" && sudo rm *.pkg.tar.zst); done
 for d in ./*/ ; do (cd "$d" && sudo rm *.pkg.tar.zst.sig); done
